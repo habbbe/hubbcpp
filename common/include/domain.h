c2b120f770cac82b517855ef3649296d2a053938
habbbe <habbbe@gmail.com>
2020-03-18 09:53:35 +0100

    Removed constructors for domain objects

--- common/include/domain.h
+++ common/include/domain.h
@@ -2,11 +2,9 @@
 
 struct action {
     std::string name, command;
-    action(std::string_view name, std::string_view command) : name{name}, command{command} {}
 };
 struct info {
     std::string name, command;
-    info(std::string_view name, std::string_view command) : name{name}, command{command} {}
 };
 
 struct separator {};
@@ -14,7 +12,6 @@ struct space {};
 
 struct syntax_error {
     std::string error;
-    syntax_error(std::string_view error) : error{error} {}
 };
 
 using entry = std::variant<action, info, separator, space, syntax_error>;
